442723ceef47f6c431b2d5864486b775c00bb321
Ramil K <hfvbkmr@mail.ru>
2024-07-17 10:04:09 +0500

    feat: test google opens in different browsers

--- __tests__/google_open_test.js
+++ __tests__/google_open_test.js
@@ -5,17 +5,26 @@ describe('Google Open', function() {
 	let driver
 
 	beforeEach(async function() {
-		driver = await new Builder().forBrowser(Browser.CHROME).build()
+		chromeDriver = await new Builder().forBrowser(Browser.CHROME).build()
+		firefoxDriver = await new Builder().forBrowser(Browser.FIREFOX).build()
 	})
 
 	test('Title is Google', async function() {
-		await driver.get('https://www.google.com/ncr')
-		const title = await driver.wait(until.titleIs('Google'), 1000)
-		expect(title).toBeTruthy()
+		await chromeDriver.get('https://www.google.com/ncr')
+		const chromeTitle = await chromeDriver.wait(until.titleIs('Google'), 1000)
+		expect(chromeTitle).toBeTruthy()
+
+		await firefoxDriver.get('https://www.google.com/ncr')
+		const firefoxTitle = await firefoxDriver.wait(until.titleIs('Google'), 1000)
+		expect(firefoxTitle).toBeTruthy()
 
 		// await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
 		// await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
 	})
 
-	afterEach(() => driver && driver.quit())
+	afterEach(() => {
+		chromeDriver && chromeDriver.quit()
+		firefoxDriver && firefoxDriver.quit()
+	}
+	)
 })
